260ab18e52e84d97ec20fa22356b6ae460845f4a
Haritha0705 <harithawikramasinha2003@gmail.com>
2026-02-05 16:36:55 +0530

    Initial commit

---
+++ src/main/java/com/eventcom360/backend/Eventcom360Application.java
@@ -0,0 +1,13 @@
+package com.eventcom360.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eventcom360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eventcom360Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/eventcom360/backend/Eventcom360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventcom360.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eventcom360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
